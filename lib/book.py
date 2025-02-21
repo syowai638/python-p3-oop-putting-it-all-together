29e3c0a7dfa6f9931c02aff998b7a0a6c7c41df7
syowai638 <cmuthini77@gmail.com>
2025-02-21 22:24:42 +0300

    OOP

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
-#!/usr/bin/env python3
+import sys
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Private variable
+        self.page_count = page_count  # Calls the setter
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
+import sys
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Private variable
+        self.size = size  # Calls the setter
+        self.condition = "New"  # Initialize condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")  # Ensures the test gets the expected print output
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")  # Match expected test output
+        self.condition = "New"  # Reset condition
